1235168a72dcbcb7d97e6b228f0e5e124713932a
Jonas Andrade (home office user) <jonas.andrade@ifpb.edu.br>
2023-04-23 10:30:46 -0300

    Added '.idea/' folder, witch has Pycharm configuration files

--- projeto/settings.py
+++ projeto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-91%68c97gm=k9s&ezp1peur096nv4qt+d+5qkt4$8s&zodj%^a'
 
 #'django-insecure-91%68c97gm=k9s&ezp1peur096nv4qt+d+5qkt4$8s&zodj%^a'
 
